5383bdd639a2b07f334cd0cd36aed3cd9be8409e
Nho-way <97704525+NickHo2048@users.noreply.github.com>
2024-11-20 09:55:42 -0800

    Update Email

--- src/App.tsx
+++ src/App.tsx
@@ -95,7 +95,7 @@ function App() {
                 <path fillRule="evenodd" clipRule="evenodd" d="M20 1C21.6569 1 23 2.34315 23 4V20C23 21.6569 21.6569 23 20 23H4C2.34315 23 1 21.6569 1 20V4C1 2.34315 2.34315 1 4 1H20ZM20 3C20.5523 3 21 3.44772 21 4V20C21 20.5523 20.5523 21 20 21H4C3.44772 21 3 20.5523 3 20V4C3 3.44772 3.44772 3 4 3H20Z" />
               </svg>
             </a>
-            <a href={`mailto:${"nickho.lv@gmail.com"}`} className="contact-button mail-icon">
+            <a href={`mailto:${"niho@ucsd.edu"}`} className="contact-button mail-icon">
               <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" strokeWidth="0">
                 <path fillRule="evenodd" clipRule="evenodd" d="M3.75 5.25L3 6V18L3.75 18.75H20.25L21 18V6L20.25 5.25H3.75ZM4.5 7.6955V17.25H19.5V7.69525L11.9999 14.5136L4.5 7.6955ZM18.3099 6.75H5.68986L11.9999 12.4864L18.3099 6.75Z"/>
               </svg>
